dac6e19f6c1f8824e293a06f2c1ee0a914de1c3f
Amirshayan1 <sh.tatari18@gmail.com>
2026-01-05 15:55:29 +0100

    feat: added implementation for parseConfigFile and the helper methods

--- utils/config/config.h
+++ utils/config/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <pugi/pugixml.hpp>
 
 /**
  * @brief EngineType defines the type of inference engine to be used
@@ -22,15 +23,31 @@ enum class ModelArch {SSD, YOLO5, YOLOV8, YOLO10, UNKNOWN};
 /**
  * @brief TestBenchConfig holds the configuration parameters for the test bench
  */
-struct TestBenchConfig
+class TestBenchConfig
 {
+  /**
+   * @brief parse engine node in config file
+   * 
+   * @param engineNode xml node
+   * @return true/false 
+   */
+  bool parseEngineNode(const pugi::xml_node& engineNode);
+
+  /**
+   * @brief parse test bench configs node
+   * @param root root xml node
+   * @return true / false
+   */
+  bool parseTestBenchConfigsNode(const pugi::xml_node root);
+
+public:
   std::string m_modelPath;                /// \var path to the model file
   std::string m_classNamesPath;           /// \var path to the class names file
-  std::string m_datasetDir;                /// \var path to the dataset directory
+  std::string m_datasetDir;               /// \var path to the dataset directory
   float m_iouThreshold;                   /// \var IOU threshold for non-max suppression
   float m_confidenceThreshold;            /// \var confidence threshold for detections
-  EngineType m_engineType;                 /// \var type of the inference engine
-  TestBenchType m_benchType;               /// \var type of the test bench
+  EngineType m_engineType;                /// \var type of the inference engine
+  TestBenchType m_benchType;              /// \var type of the test bench
   ModelArch m_arch;
 
   /**
